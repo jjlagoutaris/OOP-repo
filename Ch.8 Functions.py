75b7507de990cb14735892897315ee0d84427cd0
jjlagoutaris <jjlagoutaris@gmail.com>
2021-11-05 19:03:52 -0400

    added files

---
+++ Ch.8 Functions.py
@@ -0,0 +1,33 @@
+# # 8-12
+#
+# def make_sandwich(**sandwich_ingredients):
+#     return sandwich_ingredients
+#
+#
+# sandwich = make_sandwich(ingredients='Lettuce, Tomatoes, Bacon')
+# print(sandwich)
+
+
+# # 8-13
+
+# def build_profile(first, last, **user_info):
+#     user_info['first_name'] = first
+#     user_info['last_name'] = last
+#     return user_info
+#
+#
+# user_profile = build_profile('John', 'Lagoutaris', location='Framingham', field='Computer Science', favorite_hobby='Climbing')
+#
+# print(user_profile)
+
+# # 8-14
+#
+# def make_car(manufacturer, model, **car_info):
+#     car_info['manufacturer'] = manufacturer
+#     car_info['model'] = model
+#     return car_info
+#
+#
+# car1 = make_car('Subaru', 'Outback', color='Red', year=1996)
+#
+# print(car1)
---
+++ CodingStandards.py
@@ -0,0 +1,5 @@
+words = ["Apotheosis", "Silhouette", "Camaraderie"]
+DELIMETER = ", "
+print(DELIMETER.join(words))
+
+# allows flexibility and scalability if words or the delimeter need to be altered
